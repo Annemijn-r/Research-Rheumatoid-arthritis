14fcf18710a20014cf4890df403b0fd837cbfa21
Annemijn-r <annemijn.rieff@hotmail.com>
2025-06-19 23:21:41 +0200

    Edited

--- RData_RA/GO_analysis_RA.R
+++ RData_RA/GO_analysis_RA.R
@@ -24,19 +24,27 @@ GO_down2 <- enrichGO(gene = down_genes2,
 
 GO_up_df2 <- as.data.frame(GO_up2)
 GO_down_df2 <- as.data.frame(GO_down2)
-
-  #Saving GO-file.
+  
+#Saving GO-file.
 write.csv2(GO_up_df2, file = "GO-analysis/Excel_results_GO/GO_enrichment_results_up.csv", row.names = FALSE)
 write.csv2(GO_down_df2, file = "GO-analysis/Excel_results_GO/GO_enrichment_results_down.csv", row.names = FALSE)
 
   #Creating barplots.
-fit_up <- plot(barplot(GO_up2, showCategory = 10)) #Top 10 will be shown
+fit_up <- plot(barplot(GO_up2, showCategory = 10) + #Top 10 will be shown
+                   scale_fill_gradientn(
+                     colours = c("#f497b6", "#90e0c4", "#b8a2d6"),
+                     name = "p value"
+                   ))
 fit_up
 png("GO-analysis/Barplot_GO/GO_Upregulated_results_plot.png", res = 250, width = 1800, height = 3200)
 print(fit_up)  
 dev.off()
 
-fit_down <- plot(barplot(GO_down2, showCategory = 10)) #Top 10 will be shown
+fit_down <- plot(barplot(GO_down2, showCategory = 10) + #Top 10 will be shown
+                   scale_fill_gradientn(
+                     colours = c("#f497b6", "#90e0c4", "#b8a2d6"),
+                     name = "p value"
+                   )) 
 fit_down
 png("GO-analysis/Barplot_GO/GO_Downregulated_results_plot.png", res = 250, width = 2000, height = 3200)
 print(fit_down)  
@@ -57,7 +65,7 @@ library(dplyr)
 library(ggplot2)
 
   #Creating point diagram with counts and hits(%).
-GO_up2@result %>%
+point_up_GO <- GO_up2@result %>%
   top_n(10, wt = -pvalue) %>%
   mutate(hitsPerc = Count * 100 / as.numeric(gsub("/.*", "", BgRatio))) %>%
   ggplot(aes(x = hitsPerc, 
@@ -66,18 +74,32 @@ GO_up2@result %>%
              size = Count)) +
   geom_point() +
   expand_limits(x = 0) +
-  labs(x = "Hits (%)", y = "GO term", colour = "p value", size = "Count")
+  scale_colour_gradientn(
+    colours = c("#90e0c4", "#b8a2d6", "#f497b6"),  
+    name = "p value") +
+  labs(x = "Hits (%)", y = "GO term upregulated", colour = "p value", size = "Count")
 
-GO_down2@result %>%
+point_up_GO
+png("GO-analysis/Dotplot_GO/GO_Upregulated_results_dotplot.png", width = 10, height = 6, units = "in", res = 300)
+print(point_up_GO)  
+dev.off()
+
+point_down_GO <- GO_down2@result %>%
   top_n(10, wt = -pvalue) %>%
   mutate(hitsPerc = Count * 100 / as.numeric(gsub("/.*", "", BgRatio))) %>%
   ggplot(aes(x = hitsPerc, 
              y = Description, 
-             colour = pvalue, 
+             colour = pvalue,
              size = Count)) +
   geom_point() +
   expand_limits(x = 0) +
-  labs(x = "Hits (%)", y = "GO term", colour = "p value", size = "Count")
+  scale_colour_gradientn(
+    colours = c("#90e0c4", "#b8a2d6", "#f497b6"),
+    name = "p value") +
+  labs(x = "Hits (%)", y = "GO term downregulated", colour = "p value", size = "Count")
 
-
+point_down_GO
+png("GO-analysis/Dotplot_GO/GO_Downregulated_results_dotplot.png", width = 8, height = 6, units = "in", res = 300)
+print(point_down_GO)  
+dev.off()
 
